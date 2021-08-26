7f2e414929d66bed97338d3cd745562754cd239e
kason <kbraley9@gmail.com>
2021-08-26 11:58:59 -0700

    refactor: Add new WeatherDay component
    Add a new React component to handle only one day.
    
    Then map over the same API data and render an array
    of the WeatherDay components.

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-export default class Weather extends React.Component {
-  constructor(props) {
-    super(props);
+class WeatherDay extends React.Component {
+  render() {
+    return (
+      <Card
+        style={{
+          width: "18rem",
+          height: "15rem",
+          margin: "10px",
+          marginTop: "30px",
+        }}
+        bg="primary"
+        text="light"
+      >
+        <Card.Header>Forecast</Card.Header>
+        <Card.Body>
+          <Card.Title>Date: {this.props.date}</Card.Title>
+          <p>Details: {this.props.description}</p>
+        </Card.Body>
+      </Card>
+    );
   }
+}
 
+export default class Weather extends React.Component {
   generateData = () => {
     return this.props.forecast.map((element) => {
       return (
-        <>
-          <Card
-            style={{
-              width: "18rem",
-              height: "15rem",
-              margin: "10px",
-              marginTop: "30px",
-            }}
-            bg="primary"
-            text="light"
-          >
-            <Card.Header>Forecast</Card.Header>
-            <Card.Body>
-              <Card.Title>Date: {element.date}</Card.Title>
-              <p>Details: {element.description}</p>
-            </Card.Body>
-          </Card>
-        </>
+        <WeatherDay data={element.date} description={element.description} />
       );
     });
   };
